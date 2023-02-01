774208aec4baa4de6cf7ef7454891ec3ae5c152a
阿明 <1763113879@qq.com>
2023-02-02 01:50:21 +0800

    404 跳转 8 测试 插件化测试

---
+++ js/docsifyReloadPlugin.js
@@ -0,0 +1,46 @@
+//default values
+var defaultOptions = {
+    reloadUrlEnable: true,
+    routerMode:   window.$docsify["routerMode"],
+}
+
+// Docsify plugin functions
+function plugin(hook, vm) {
+
+    if (!defaultOptions.reloadUrlEnable) {
+        return
+    }
+   if (defaultOptions.routerMode != 'history') {
+        return
+    }
+    hook.afterEach(function (html) {
+            <!-- 跳转页 -->
+               var referrer = document.referrer;
+               var url = window.location.href
+               console.log(document.referrer) ;
+               console.log(referrer) ;
+
+              <!--  有上个url    -->
+                if (referrer) {
+                    <!--  不是当前页面    -->
+                  if(url != referrer){
+                          <!--  不是404页面    -->
+                      if(referrer.indexOf("404.html") == -1){
+                            var spath = referrer.substring(window.location.origin.length);
+                            var selecturl = "a[href=\'"+spath+"\']";
+                            console.log("模拟跳转");
+                            console.log(selecturl);
+                            vm.dom.find(selecturl).click();
+                      }
+                  }
+
+                };
+    })
+}
+
+// Docsify plugin options
+window.$docsify["docsifyReloadPlugin"] = Object.assign(
+    defaultOptions,
+    window.$docsify["docsifyReloadPlugin"]
+)
+window.$docsify.plugins = [].concat(plugin, window.$docsify.plugins)
