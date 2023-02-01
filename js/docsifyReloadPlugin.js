1f8a5ed09db36faa346a20d63484fa13efd32d86
阿明 <1763113879@qq.com>
2023-02-02 02:25:42 +0800

    404 跳转 14 测试 init

--- js/docsifyReloadPlugin.js
+++ js/docsifyReloadPlugin.js
@@ -13,7 +13,7 @@ function plugin(hook, vm) {
    if (defaultOptions.routerMode != 'history') {
         return
     }
-    hook.doneEach(function () {
+    hook.init(function () {
             <!-- 跳转页 -->
                var referrer = document.referrer;
                var url = window.location.href
