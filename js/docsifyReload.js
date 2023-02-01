87677ebf7d1c0dd2f51fed11c0c6a5e8f1bb617a
阿明 <1763113879@qq.com>
2023-02-02 00:51:59 +0800

    404 跳转 6  测试 ；延迟加载js

---
+++ js/docsifyReload.js
@@ -0,0 +1,27 @@
+
+<script>
+<!-- 跳转页 -->
+   var referrer = document.referrer;
+   var url = window.location.href
+   console.log(document.referrer) ;
+   console.log(referrer) ;
+
+  <!--  有上个url    -->
+    if (referrer) {
+        <!--  不是当前页面    -->
+      if(url != referrer){
+              <!--  不是404页面    -->
+          if(referrer.indexOf("404.html") == -1){
+                var spath = referrer.substring(window.location.origin.length);
+                var selecturl = "a[href=\'"+spath+"\']";
+                console.log("模拟跳转");
+                console.log(selecturl);
+                Docsify.dom.find(selecturl).click();
+
+          }
+      }
+
+    }
+
+
+</script>
