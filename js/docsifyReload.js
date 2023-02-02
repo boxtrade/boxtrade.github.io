88ba309cc64f3c762716174cf29cf9a8ae4a5711
阿明 <1763113879@qq.com>
2023-02-02 10:34:06 +0800

    404 跳转 15 测试 打印

--- js/docsifyReload.js
+++ js/docsifyReload.js
@@ -1,15 +1,14 @@
 <!-- 跳转页 -->
    var referrer = document.referrer;
    var url = window.location.href
-   console.log(document.referrer) ;
-   console.log(referrer) ;
-
   <!--  有上个url    -->
     if (referrer) {
         <!--  不是当前页面    -->
       if(url != referrer){
               <!--  不是404页面    -->
           if(referrer.indexOf("404.html") == -1){
+                 console.log(document.referrer) ;
+                 console.log(referrer) ;
                 var spath = referrer.substring(window.location.origin.length);
                 var selecturl = "a[href=\'"+spath+"\']";
                 console.log("模拟跳转");
@@ -19,4 +18,4 @@
           }
       }
 
-    }
+        }
